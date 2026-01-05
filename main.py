cc6d606108c90ddc7f45b84fdc61ce257f6dc369
Cristian <cristian@bratash.net>
2026-01-05 18:56:44 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+city = "Torrevieja"
+print(city + " is very good city yes bro")
+
+current_year = 2026
+print("now is " + str(current_year) + " year")
+
+birth_year = int(input("when was u born gng answer here ---> "))
+print(birth_year)
+
+age = (current_year - birth_year)
+print(f"this year you will be {age} years old")
+print(f"in the year 2067 you will be {2067 - birth_year}")
