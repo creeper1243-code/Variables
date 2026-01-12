c5d3967d988553b26f73ad04fce8945b7f8871a5
Cristian <cristian@bratash.net>
2026-01-12 18:34:11 +0100

    try boolean

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ print("now is " + str(current_year) + " year")
 birth_year = int(input("when was u born gng answer here ---> "))
 print(birth_year)
 
-age = (current_year - birth_year)
+age = current_year - birth_year
 print(f"this year you will be {age} years old")
 print(f"in the year 2067 you will be {2067 - birth_year}")
+
+boolean = 6 < 1
+print(boolean)
